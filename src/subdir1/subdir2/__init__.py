c84a161d9938b0ef607c45d8c624a05ae36ec501
michael0703 <liaoweiskewer0703@gmail.com>
2021-01-27 15:45:24 +0800

    add subdir 2

---
+++ src/subdir1/subdir2/__init__.py
@@ -0,0 +1,4 @@
+'''
+this is the subfolder and the depth is 2
+
+'''
---
+++ src/subdir1/subdir2/run2.py
@@ -0,0 +1,6 @@
+def run2():
+    '''
+    the main use of this function is to test
+    if sphinx can recursively generate api doc
+    '''
+    print(4, 5)
