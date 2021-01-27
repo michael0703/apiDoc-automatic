8761c2fb494ec4b8080aef581d70bfa6426d8c5e
michael0703 <liaoweiskewer0703@gmail.com>
2021-01-27 15:15:07 +0800

    modify build script

--- buildapi.sh
+++ buildapi.sh
@@ -1,9 +1,7 @@
 rm -rf doc
 mkdir doc
 cd doc
-echo `ls -al`
 sphinx-quickstart -q -p api_test -a michael -v 3.7 --ext-autodoc --ext-doctest --ext-intersphinx --ext-todo --ext-coverage --ext-mathjax --sep --extensions=sphinx.ext.napoleon,sphinx.ext.autosummary
-# echo "extensions.append('sphinx.ext.napoleon')" >> source/conf.py
 echo "import os" >> source/conf.py
 echo "import sys" >> source/conf.py
 echo "autosummary_generate = True" >> source/conf.py
