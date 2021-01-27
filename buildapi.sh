00b14b1b90eff8b40b7750f19efb66a77051a67a
michael0703 <liaoweiskewer0703@gmail.com>
2021-01-27 15:19:06 +0800

    use another way create conf

--- buildapi.sh
+++ buildapi.sh
@@ -2,10 +2,11 @@ rm -rf doc
 mkdir doc
 cd doc
 sphinx-quickstart -q -p api_test -a michael -v 3.7 --ext-autodoc --ext-doctest --ext-intersphinx --ext-todo --ext-coverage --ext-mathjax --sep --extensions=sphinx.ext.napoleon,sphinx.ext.autosummary
-echo "import os" >> source/conf.py
-echo "import sys" >> source/conf.py
-echo "autosummary_generate = True" >> source/conf.py
-echo "autoclass_content = 'both'" >> source/conf.py
-echo "sys.path.insert(0, os.path.abspath('../../src'))" >> source/conf.py
+# echo "import os" >> source/conf.py
+# echo "import sys" >> source/conf.py
+# echo "autosummary_generate = True" >> source/conf.py
+# echo "autoclass_content = 'both'" >> source/conf.py
+# echo "sys.path.insert(0, os.path.abspath('../../src'))" >> source/conf.py
+cp conftemplate.py ./doc/source/conf.py
 sphinx-apidoc -o ./source ../src/
 make clean && make html
